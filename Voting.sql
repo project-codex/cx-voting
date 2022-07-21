08ee5366a32c06ff39ef32863ca42b9c825d5083
Roberts Bauers <rrbauerss@gmail.com>
2022-07-22 02:36:00 +0300

    Create Voting.sql

---
+++ Voting.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `players` ADD COLUMN `hasvoted` TINYINT NULL DEFAULT '0'
+
+CREATE TABLE IF NOT EXISTS `election` (
+  `name` varchar(50) DEFAULT NULL,
+  `party` varchar(50) DEFAULT NULL,
+  `votes` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
