ca266964bc8f010d80efb8ac1c905ca05e9ebb59
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-07-24 21:02:18 +0300

    Update Voting.sql

--- Voting.sql
+++ Voting.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `players` ADD COLUMN `hasvoted` TINYINT NULL DEFAULT '0'
+ALTER TABLE `players` ADD COLUMN `hasvoted` TINYINT NULL DEFAULT '0';
 
 CREATE TABLE IF NOT EXISTS `election` (
   `name` varchar(50) DEFAULT NULL,
